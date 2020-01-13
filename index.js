44832a8b145e84b6aaa3c827075487cca01e011b
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-13 10:42:29 +0100

    wip

---
+++ index.js
@@ -0,0 +1,9 @@
+// install express and cors (npm packages)
+// STEP 1: flesh out a dummy server
+// STEP 2: flesh out the five endpoints (will use those helpers)
+
+// imports
+const express = require('express') // commonjs equivalent to "import express from 'express'"
+const cors = require('cors')
+
+// instantiate an express app
