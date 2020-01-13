6269ec78b9f429207f1d5efbe28cc000f0ed83b5
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-13 12:15:42 +0100

    wiop

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.delete("/hubs/:id", (req, res) => {
     })
 });
 
-app.put("/hubs/:id/:friend/:gaga", async (req, res) => {
+app.put("/hubs/:id", async (req, res) => {
   // PUT a hub by id using the request body
   const { id } = req.params
   const replacementHub = req.body
